f1128c9ed13d12102a389470ab3ae4121cb5ced8
mario acevedo <jun_acevedo415@yahoo.com>
2020-07-04 22:24:18 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,17 @@
 class Person
-  def initialize(name)
+
+  def name=(name)
     @name = name
   end
- 
   def name
     @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
   end
 end
